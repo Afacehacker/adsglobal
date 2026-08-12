7e90c08ef257553d9bef8c599069e25284107691
Afacehacker <afaceabolade@gmail.com>
2026-08-12 20:40:49 +0100

    Trigger automated Render deployment for latest backend features and OTP verification

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const app = require('./app');
 const connectDB = require('./config/db');
 
+// Render Deployment Trigger: 2026-08-12
 const PORT = process.env.PORT || 5000;
 
 // Start Server
